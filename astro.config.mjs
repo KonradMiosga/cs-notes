8cc66b42eb49d7b6b9137d419e5ebb39bb1bb195
Konrad Miosga <konrad.miosga@gmail.com>
2023-11-28 19:41:28 +0100

    ivunjmk

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://konradmiosga.github.io',
-    base: '/cs-notes',
+    base: '/cs-notes/',
     vite: {
         resolve: {
             preserveSymlinks: true
