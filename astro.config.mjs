277b5b2274d056e819384766e824e0c380307f79
Konrad Miosga <konrad.miosga@gmail.com>
2023-12-09 19:39:35 +0100

    color schema

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,5 +11,18 @@ export default defineConfig({
     },
     build: {
         format: 'file'
-    }
+    },
+    markdown: {
+        shikiConfig: {
+            // Choose from Shiki's built-in themes (or add your own)
+            // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+            theme: 'material-theme-darker',
+            // Alternatively, provide multiple themes
+            // https://github.com/antfu/shikiji#lightdark-dual-themes
+            // experimentalThemes: {
+            //     light: 'github-light',
+            //     dark: 'github-dark',
+            // },
+        },
+    },
 });
